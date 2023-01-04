099ceda1933d69703ff228a5b036e6c48ec5d142
Daniel Alexander <daniel.llumiquinga@hotmail.com>
2023-01-04 11:05:44 -0500

    Add req validator

---
+++ middlewares/validatorHandler.js
@@ -0,0 +1,10 @@
+export const validatorHandler = (schema, property) => {
+  return (req, res, next) => {
+    const data = req[property];
+    const { error } = schema.validate(data, { abortEarly: false });
+    if (error) {
+      next(res.status(400).json({ error: error.message }));
+    }
+    next();
+  };
+};
