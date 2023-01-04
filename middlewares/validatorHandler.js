67955e5eb057979421eb7a3d66548b4ef7cead53
Daniel Alexander <daniel.llumiquinga@hotmail.com>
2023-01-04 11:23:42 -0500

    GH repo

--- middlewares/validatorHandler.js
+++ middlewares/validatorHandler.js
@@ -1,3 +1,11 @@
+/**
+ * It takes a schema and a property, and returns a function that takes a request, response, and next
+ * function, and validates the request's property against the schema
+ * @param schema - The schema that we want to validate against.
+ * @param property - The property of the request object that contains the data to be validated.
+ * @returns A function that takes in a schema, property, and returns a function that takes in req, res,
+ * and next.
+ */
 export const validatorHandler = (schema, property) => {
   return (req, res, next) => {
     const data = req[property];
